90bb9cfd286d1f8114aea0f2b83c88a4f965694a
dcantero <dan@dcantero.com>
2023-08-07 17:04:25 -0400

    added index.html

---
+++ js/index.js
@@ -0,0 +1,20 @@
+var mobileMenuIcon = document.querySelector(".mobile-menu-icon");
+var mobileMenu = document.querySelector(".mobile-menu-list");
+var siteContent = document.querySelector("#site-content");
+
+mobileMenuIcon.addEventListener("click", function () {
+
+    if(!mobileMenu.classList.contains("mobile-menu-list-active")){
+    mobileMenu.classList.add("mobile-menu-list-active");
+    siteContent.classList.add("site-content-active");
+    mobileMenu.classList.remove("mobile-menu-list-inactive");
+        siteContent.classList.remove("site-content-inactive");
+    } else{
+      
+        mobileMenu.classList.add("mobile-menu-list-inactive");
+        siteContent.classList.add("site-content-inactive");
+        mobileMenu.classList.remove("mobile-menu-list-active");
+        siteContent.classList.remove("site-content-active");
+       
+    }
+});
