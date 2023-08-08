3de65b6ce9df3687177176de68b6078daa57636e
dcantero <dan@dcantero.com>
2023-08-07 22:29:39 -0400

    updated index, added multple pages

---
+++ js/map.js
@@ -0,0 +1,8 @@
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 39.737629, lng: -75.002522 },
+        zoom: 13,
+        mapId: '10bceab29bd1309e',
+        disableDefaultUI: true
+    });
+    }
